2f44ee1332e1b02684a1a6ae7e97c2adb2a48efa
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-06-30 22:57:12 +0200

    first trial on html in django

--- PhysioWat/PhysioWat/views.py
+++ PhysioWat/PhysioWat/views.py
@@ -2,6 +2,8 @@ __author__ = 'federico'
 from django.shortcuts import render
 
 def index(request):
-    template = 'PhysioWat/index.html'
+    template = 'PhysioWat/footer.html'
     context = {'a': 1234, 'name':"txt"}
     return render(request, template, context)
+
+
