3b5a3c95d92488e60783cc9d52ae9375d58f098c
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-07-01 00:52:21 +0200

    added some fancy stuff

--- PhysioWat/PhysioWat/views.py
+++ PhysioWat/PhysioWat/views.py
@@ -2,8 +2,14 @@ __author__ = 'federico'
 from django.shortcuts import render
 
 def index(request):
-    template = 'PhysioWat/footer.html'
+    template = 'PhysioWat/index.html'
     context = {'a': 1234, 'name':"txt"}
     return render(request, template, context)
+
+def trialpage(request):
+	#this function was added just to see how the django - html works. Begging the pardon of the db team, i may put on more functions like this. just don't worry (for now :) )
+	template = 'PhysioWat/index.html'
+	context = {'l':['a','b','c']}
+	return render(request, template, context)
 
 
