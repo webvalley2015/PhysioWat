bcd86e2abc520ab4d6e489841f7c62bb2d085943
federicoFerri <federicoferri97@gmail.com>
2015-06-30 19:18:15 +0200

    starting auth again

--- PhysioWat/PhysioWat/views.py
+++ PhysioWat/PhysioWat/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     template = 'PhysioWat/index.html'
-    context = {'a': 1234, 'name':"ema"}
+    context = {'a': 1234, 'name':"txt"}
     return render(request, template, context)
