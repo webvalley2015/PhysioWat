2834616f413d9ad7d6d0b221b9c8d2dff127caa9
covix <luca.coviello@gmail.com>
2015-06-30 18:51:14 +0200

    Update views.py

--- PhysioWat/PhysioWat/views.py
+++ PhysioWat/PhysioWat/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    html_file = 'PhysioWat/index.html'
+    template = 'PhysioWat/index.html'
     context = {'a': 1234}
     return render(request, html_file, context)
