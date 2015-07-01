706b4500a28381849e1f16e3635d5a04b70ad13e
federicoFerri <federicoferri97@gmail.com>
2015-07-01 14:47:27 +0200

    adding upload app...

--- PhysioWat/uploader/views.py
+++ PhysioWat/uploader/views.py
@@ -12,4 +12,4 @@ def home(request):
     else:
         img=UploadForm()
     images=Upload.objects.all()
-    return render(request,'home.html',{'form':img,'images':images})
+    return render(request, 'uploader/home.html',{'form':img,'images':images})
