151dc19cec636d94c37f5cab37cdb12c4fc97b97
Valerie Ding <dingv@stanford.edu>
2015-07-01 16:53:36 +0200

    corrected misspelling of zeros

--- Classification/filegenerate.py
+++ Classification/filegenerate.py
@@ -10,9 +10,9 @@ filenames = ['claire1.txt'] # first trial from Claire's wrist
 data_all = []
 data_mean = []          
 data_var = []
-data_combined = np.zeroes(9)
-data_mean_combined = np.zeroes(9)
-data_var_combined = np.zeroes(9)
+data_combined = np.zeroes((9,))
+data_mean_combined = np.zeros(9)
+data_var_combined = np.zeros(9)
 
 WINDOW_SIZE = 50
 STEP = 25
@@ -20,24 +20,24 @@ STEP = 25
 # N = len(filenames)
 
 data_all.append(np.loadtxt(filenames(i))
-data_combined = np.vstack((data_combined, data_all[i]))
+# data_combined = np.vstack((data_combined, data_all[i]))
 [row, col] = data_all[i].shape
-_mean = np.zeros(( len(range(0, row, STEP)) , col ))
-_var = np.zeros(( len(range(0, row, STEP)) , col ))
+mean = np.zeros((len(range(0, row, STEP)) , col))
+var = np.zeros((len(range(0, row, STEP)) , col))
 
 for i_col in range(col):
 	idx = 0
 	for i_row in range(0, row, STEP):
 	# compute mean of window elements
-	_mean[idx,i_col] = np.mean(data_all[i][i_row:i_row+WINDOW_SIZE-1,i_col])
-        _var[idx,i_col] = np.var(data_all[i][i_row:i_row+WINDOW_SIZE-1,i_col])
+	mean[idx,i_col] = np.mean(data_all[i][i_row:i_row+WINDOW_SIZE-1,i_col])
+        var[idx,i_col] = np.var(data_all[i][i_row:i_row+WINDOW_SIZE-1,i_col])
         idx += 1
-data_mean.append(_mean)
-data_mean_combined = np.vstack((data_mean_combined, data_mean[i]))
 
-data_var.append(_var)
-data_var_combined = np.vstack((data_var_combined, data_var[i]))
+data_mean.append(mean)
+# data_mean_combined = np.vstack((data_mean_combined, data_mean[i]))
+
+data_var.append(var)
+# data_var_combined = np.vstack((data_var_combined, data_var[i]))
 
 np.save('data_mean', data_mean)
 np.save('data_var', data_var)
-
