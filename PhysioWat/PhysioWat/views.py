2c7c24c9c1560ad2751f787ddccc99a96bdbc872
Daniele Zanotelli <dazano@gmail.com>
2015-07-01 21:57:29 +0200

    fixed home

--- PhysioWat/PhysioWat/views.py
+++ PhysioWat/PhysioWat/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 def index(request):
     template = 'PhysioWat/index.html'
     context = {'a': 1234, 'name':"txt"}
-    return "ciaaao"
-    #return render(request, template, context)
+    return render(request, template, context)
 
 def trialpage(request):
 	#this function was added just to see how the django - html works. Begging the pardon of the db team, i may put on more functions like this. just don't worry (for now :) )
