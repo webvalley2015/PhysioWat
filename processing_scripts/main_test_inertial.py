8546476effdc287df5915879cad04b5c1e7b9184
Riccardo Mancini <rickyman7@gmail.com>
2015-07-01 17:27:00 +0200

    SIGNAL PROCESSING TOOLS:merge conflict resolved (maybe)

--- processing_scripts/main_test_inertial.py
+++ processing_scripts/main_test_inertial.py
@@ -7,7 +7,7 @@ filename="./data/Accelerometer.csv"
 lables=["timestamp", "accX", "accY", "accZ"]
 
 data=tools.load_file_pd(filename)
-data=inertial.convert_units(data, lables[1:], coeff=0.85)
+data=inertial.convert_units(data, lables[1:], coeff=2*9.81/128)
+print data
 feats=inertial.extract_features_acc(data, fsamp=32, col_acc=lables[1:])
-print feats.columns
-
+print len(feats.columns)
