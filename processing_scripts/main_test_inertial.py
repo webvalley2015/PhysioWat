9bcb0ebb0a72459f4099872ec96f1195d5e213e1
Riccardo Mancini <rickyman7@gmail.com>
2015-07-01 19:18:25 +0200

    dinner time

--- processing_scripts/main_test_inertial.py
+++ processing_scripts/main_test_inertial.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filename="./data/claire_Control.txt"
-lables=["timestamp", "packet","accX", "accY", "accZ", "gyrX", "gyrY", "gyrZ", "magX", "magY", "magZ"]
+filename="./data/claire_ASD.txt"
+lables=["timestamp", "packetCounter","accX", "accY", "accZ", "gyrX", "gyrY", "gyrZ", "magX", "magY", "magZ"]
 lables_acc=["accX", "accY", "accZ"]
 lables_gyr=["gyrX", "gyrY", "gyrZ"]
 lables_mag=["magX", "magY", "magZ"]
@@ -25,13 +25,25 @@ data=inertial.convert_units(data, lables_acc, coeff=sensAccCoeff)
 data=inertial.convert_units(data, lables_gyr, coeff=sensGyrCoeff)
 data=inertial.convert_units(data, lables_mag, coeff=sensMagCoeff)
 
-# plt.figure(1)
-# plt.plot(data.timestamp, data[lables_acc])
-# plt.figure(2)
-# plt.plot(data.timestamp, data[lables_gyr])
-# plt.figure(3)
-# plt.plot(data.timestamp, data[lables_mag])
-# plt.show()
+plt.figure(1)
+plt.plot(data.timestamp, data[lables_acc])
+plt.legend(lables_acc)
+plt.xlabel("Time (ms)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.title("Accelerometer")
+plt.figure(2)
+plt.plot(data.timestamp, data[lables_gyr])
+plt.legend(lables_gyr)
+plt.xlabel("Time (ms)")
+plt.ylabel("Angular Speed (degree/s)")
+plt.title("Gyroscope")
+plt.figure(3)
+plt.plot(data.timestamp, data[lables_mag])
+plt.legend(lables_mag)
+plt.title("Magnetometer")
+plt.ylabel("uT")
+plt.xlabel("Time (ms)")
+plt.show()
 
 # data=inertial.convert_units(data, lables[1:], coeff=empaticaAccCoeff)
 # print data
@@ -41,5 +53,5 @@ feats_gyr=inertial.extract_features_gyr(data, fsamp=sensfsamp, col_gyr=lables_gy
 feats_mag=inertial.extract_features_mag(data, fsamp=sensfsamp, col_mag=lables_mag, WINLEN=1000, WINSTEP=500)
 feats=pd.concat([feats_acc, feats_gyr, feats_mag], axis=1)
 
-feats.to_csv("./output/feat_"+filename[7:-4]+".csv")
+# feats.to_csv("./output/feat_"+filename[7:-4]+".csv")
 
