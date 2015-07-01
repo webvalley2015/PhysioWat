6d82af21fbe4a4b39568d8ec5e067b4a8fc1bb6e
Riccardo Mancini <rickyman7@gmail.com>
2015-07-01 11:05:04 +0200

    SIGNAL PRIOCESSING: added json writing function for graphic designers

--- processing_scripts/main_test_GSR.py
+++ processing_scripts/main_test_GSR.py
@@ -2,6 +2,7 @@ import tools
 import GSR
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 import pandas as pd
 
 filename="./data/GSR.csv"
@@ -17,7 +18,16 @@ gsr_data = tools.load_file(filename)
 
 t_driver, driver, phasic_d, tonic_d = GSR.estimate_drivers(gsr_data[:,0], gsr_data[:,1], T1, T2, MX, DELTA)
 
+#plt.figure()
+#plt.plot(gsr_data[:,0], gsr_data[:,1])
+#plt.show()
+
 plt.figure()
-plt.plot(t_driver, np.c_[driver, tonic_d])
-plt.legend(["Driver", "Tonic"])
+plt.plot(t_driver, np.c_[driver, tonic_d, phasic_d])
+plt.legend(["Driver", "Tonic", "Phasic"])
 plt.show()
+
+pha_processed = GSR.processPSR(phasic_d, t_driver, DELTA)
+features = GSR.PSRindexes(pha_processed)
+
+tools.prepare_json_to_plot([driver, tonic_d, phasic_d],["Driver", "Tonic", "Phasic"])
