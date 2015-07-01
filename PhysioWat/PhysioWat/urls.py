2f251d5b8921ddb925850ee4a9b74cfb8103d840
federicoFerri <federicoferri97@gmail.com>
2015-07-01 10:52:27 +0200

    Adding template base dirs

--- PhysioWat/PhysioWat/urls.py
+++ PhysioWat/PhysioWat/urls.py
@@ -10,7 +10,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
+    1. Add an import:  from blog import urls as blog_urlsu
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
