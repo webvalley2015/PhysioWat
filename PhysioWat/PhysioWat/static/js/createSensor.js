42f92916beeb9ea3ba9c88d68d4001a5c2a28c8d
federicoFerri <federicoferri97@gmail.com>
2015-07-02 16:27:33 +0200

    new sensors features

--- PhysioWat/PhysioWat/static/js/createSensor.js
+++ PhysioWat/PhysioWat/static/js/createSensor.js
@@ -12,5 +12,17 @@ function getValue() {
     for (var c = boxNumb + 1; c <= 25; c++) {
         document.getElementById("nameIn" + c).style.display = 'none';
         document.getElementById("typeIn" + c).style.display = 'none';
+        document.getElementById("extraIn" + c).style.display = 'none';
     }
 }
+
+function addField(num) {
+    var e = document.getElementById("typeIn" + num);
+    var option = e.options[e.selectedIndex].value;
+   if (option == "empty"){
+       document.getElementById("extraIn" + num).style.display = 'block';
+   }else{
+       document.getElementById("extraIn" + num).style.display = 'none';
+   }
+}
+
