53d1d4b974b22da868ef291727e416b9f180dfe4
Riccardo Mancini <rickyman7@gmail.com>
2015-07-02 15:35:27 +0200

    added windowing

---
+++ processing_scripts/windowing.py
@@ -0,0 +1,14 @@
+'''
+Functions for windowing
+'''
+import numpy as np
+import pandas as pd
+
+def get_windows_contiguos(labels, WINLEN, WINSTEP):
+    pass
+
+def get_windows_no_mix(labels, WINLEN, WINSTEP):
+    pass
+
+def get_windows_full_label(labels, WINLEN, WINSTEP):
+    pass
