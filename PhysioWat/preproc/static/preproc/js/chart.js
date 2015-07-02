f77e3cb8c91fe8d7a6ba5da21b2f419f9fcfb5f0
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-07-02 10:35:29 +0200

    added graph with exp data

--- PhysioWat/preproc/static/preproc/js/chart.js
+++ PhysioWat/preproc/static/preproc/js/chart.js
@@ -4,44 +4,37 @@ console.log("file javascript loaded");
  *  lineg raph of the views, which is, read the json e return data
  */
  
- var data = {};
+
 ///PICK DATA
-function ajax_get_data() {
-	var url = '/preproc/linegraph_getdata/'
+function ajax_get_data( where="chartbox" ) {
+	var url = '/preproc/linegraph_getdata/';
 	
 	// $ == jQuery, è la stessa variabile !!!!!!
 	$.ajax(url,    
 		   {cache: false,
-			async: false,
+			///async: false,
 			dataType: "json",
 			error: function () {
 						console.log("errore prendendo i dati!!");
 				   },
 			method: "GET",
-			success: function (data_recieved) {
-						console.log("got the data!!");
-						create_graph(data_recieved);
+			success: function (data_received) {
+						create_graph(data_received, where);
 				   },  
 		   }
 	);  
 }
 
 
-function create_graph(data)
+function create_graph(data, where)
 {
-	if (data['chart'] == undefined)
-	{
+	if (data['chart'] == undefined) {
 		data['chart'] = {};	
 	}
 	
-	data['chart']['renderTo'] = "chartbox";
+	data['chart']['renderTo'] = where;
 	
 	
 	var chart = new Highcharts.Chart(data);
 	
 }
-
-$(document).ready(function() {
-	ajax_get_data();
-	}
-);
