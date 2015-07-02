95d70097695e40cf235ebb6cc19b58b99030bdda
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-07-02 11:48:33 +0200

    added chart title

--- PhysioWat/preproc/graphs.py
+++ PhysioWat/preproc/graphs.py
@@ -5,12 +5,16 @@ from django.conf import settings
 
 class linegraph(HighChartsLineView):
 	
-	def get_data(self):
+	
+	def get_data(self, title=""):
 		myfile=os.path.join(settings.BASE_DIR, "preproc/graph.json") #filename
 		f = open(myfile, "r")
 		data = json.load(f)
 		f.close()
-		#data = super()
+		# if title was provided requesting the url, use it as chart
+		# title, otherwise hope to find title in json
+		if title:
+			data['title'] = {}
+			data['title']['text'] = title
+		
 		return data
-
-
