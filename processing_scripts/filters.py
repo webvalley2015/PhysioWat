b4efb0064f1294547c9d045d3c057b950274727b
Riccardo Mancini <rickyman7@gmail.com>
2015-07-02 23:23:44 +0200

    added remove spikes up-to-date version

--- processing_scripts/filters.py
+++ processing_scripts/filters.py
@@ -6,12 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import filter_design as fd
 
-def smoothGaussian(X,sigma=5):
+def smoothGaussian(X,sigma=5, switch=True):
     """
     SIGNAL_OUT = smoothGaussian(SIGNAL_IN,SIGMA=5):
 
     Gaussian smooting by convolution with a gaussian window with sigma=SIGMA
     """
+    if not switch:
+        return X
     window = gaussian(sigma * 10+1, sigma)
     smoothed = convolve(X, window, 'same')
 
