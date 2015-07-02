a81971000a0405011b9675647d844c01317c191a
Stephen Mylabathula <mylo777@icloud.com>
2015-07-02 12:36:52 +0200

    Stephen

--- PhysioWat/designer/forms.py
+++ PhysioWat/designer/forms.py
@@ -5,6 +5,6 @@ from django import forms
 # FileUpload form class.
 class SensorDesignerForm(forms.Form):
     Name = forms.CharField()
-    Sensors = forms.IntegerField(min_value=1, widget=forms.NumberInput(attrs={
+    Sensors = forms.IntegerField(min_value=1, max_value=25, widget=forms.NumberInput(attrs={
         'onClick': "getValue()",
     }))
