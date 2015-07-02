8c06dd60411ca4a823b684e746df10f2c559e9b6
filosi <filosi@fbk.eu>
2015-07-02 22:32:48 +0200

    Remove default key in preproc form.py

--- PhysioWat/preproc/forms.py
+++ PhysioWat/preproc/forms.py
@@ -14,38 +14,44 @@ CHOICES=[("butter", "Butterworth"),
         ("ellip", "Couer/Ellip"),
         ("none", "Filter OFF")]
 
+
 class filter(forms.Form):
-    passFr = forms.FloatField(min_value = 0)
-    stopFr = forms.FloatField(min_value = 0)
-    LOSS = forms.FloatField(min_value = 0)
-    ATTENUATION = forms.FloatField(min_value = 0)
+    passFr = forms.FloatField(min_value=0)
+    stopFr = forms.FloatField(min_value=0)
+    LOSS = forms.FloatField(min_value=0)
+    ATTENUATION = forms.FloatField(min_value=0)
     filterType = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
+
 
 class remove_spike(forms.Form):
     remove_spike = forms.BooleanField()
-    TH = forms.FloatField(min_value = 0)
+    TH = forms.FloatField(min_value=0)
+
 
 class GSR(forms.Form):
-    T1 = forms.FloatField(min_value = 0, default = 0.75)
-    T2 = forms.FloatField(min_value = 0, default = 2)
-    MX = forms.FloatField(min_value = 0, default = 1)
-    DELTA_PEAK = forms.FloatField(min_value = 0, default = 0.02)
-    k_near = forms.IntegerField(min_value = 0, default = 5)
-    grid_size = forms.IntegerField(min_value = 0, default = 5)
-    s = forms.FloatField(min_value = 0, default = 0.2)
+    T1 = forms.FloatField(min_value=0.0)
+    T2 = forms.FloatField(min_value=0.0)
+    MX = forms.FloatField(min_value=0.0)
+    DELTA_PEAK = forms.FloatField(min_value=0)
+    k_near = forms.IntegerField(min_value=0)
+    grid_size = forms.IntegerField(min_value=0)
+    s = forms.FloatField(min_value=0)
+
 
 class EKG(forms.Form):
-    delta = forms.FloatField(min_value = 0, default = 0.2)
-    minFr = forms.FloatField(min_value = 0, default = 40)
-    maxFr = forms.FloatField(min_value = 0, default = 200)
+    delta = forms.FloatField(min_value=0)
+    minFr = forms.FloatField(min_value=0)
+    maxFr = forms.FloatField(min_value=0)
+
 
 class BVP(forms.Form):  #Uguale a quello sopra ma cambia un default
-    delta = forms.FloatField(min_value = 0, default = 1)
-    minFr = forms.FloatField(min_value = 0, default = 40)
-    maxFr = forms.FloatField(min_value = 0, default = 200)
+    delta = forms.FloatField(min_value=0)
+    minFr = forms.FloatField(min_value=0)
+    maxFr = forms.FloatField(min_value=0)
+
 
 class inertial(forms.Form):
-    coeff = forms.FloatField(default = 1)
+    coeff = forms.FloatField()
 
 
 
