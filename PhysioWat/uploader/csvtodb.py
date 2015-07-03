5a563aeb2879cc27498fe9a17a96c323b50cbccf
Illedran <nrd.nardelli@gmail.com>
2015-07-03 14:53:39 +0200

    Refactored models and data import

--- PhysioWat/uploader/csvtodb.py
+++ PhysioWat/uploader/csvtodb.py
@@ -2,19 +2,23 @@ __author__ = 'andrew'
 
 import csv
 
-from PhysioWat.models import Recording
-from datetime import datetime
-from django.db import connection
+from PhysioWat.PhysioWat.models import Recording, SensorData
+# from django.db import connection
 
 
-def putintodb(fname, devicename):
+def putintodb(fname, dvname):t
     csvreader = csv.reader(fname[0], delimiter=',')
     dictky = csvreader.next()
-    #Recording(experimentid=1, devicename='IMU', dictkeys=dictky, ts=datetime.now()).save()
+    r = Recording(experiment_id=1, devicename=dvname, dictkeys=dictky)
+    r.save()
 
     for row in csvreader:
+        import_dict = dict.fromkeys(dictky, row)
+        SensorData(recording_id=r.id(), store=import_dict)
         # raw query for each csv line
-        cursor = connection.cursor()
-        cursor.execute('INSERT INTO sensor_data (recordingid,store) VALUES (1,hstore(%s,%s))', [dictky,row])
+        # cursor = connection.cursor()
+        # cursor.execute('INSERT INTO sensor_data (recordingid, store) VALUES (1,hstore(%s,%s))', [dictky,row])
+        # create a new record, referencing the newly added recording and a hstore by passing
+        # the keys and the values with two different lists
         # results = cursor.fetchall()
     return 0
