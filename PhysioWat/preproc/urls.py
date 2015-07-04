8b48f932f7c719ea094c742b41994b9f88d8deeb
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-07-04 18:00:29 +0200

    trying to fix the mess 2

--- PhysioWat/preproc/urls.py
+++ PhysioWat/preproc/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 from .graphs import linegraph
-#from .views import show_chart, preproc_settings
+from .views import show_chart, preproc_settings,select_experiment, select_experiment_no_use
 
 #the method as_view(), from the library, will  call get_data(), written, and mabye other things.
 
 urlpatterns = patterns('preproc.views',
-    url(r'^experiments/$', 'select_experiment', name='experiment_selector'),
+    url(r'^experiments/$', view=select_experiment, name='experiment_selector'),
 	url(regex='^settings/$', view=preproc_settings, name="settings_preproc"),
     url(regex='^linegraph_getdata/$', view=linegraph.as_view(), name="chart_getdata" ),
     url( regex='^linegraph_getdata/(?P<title>\w+)/$', view=linegraph.as_view(), name="chart_getdata" ),
