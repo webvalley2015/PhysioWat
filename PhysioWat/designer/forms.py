2a33cd39809b7ae7b4a4bbc008739742e7987f2a
federicoFerri <federicoferri97@gmail.com>
2015-07-04 11:23:30 +0200

    experiment creator working with all the checks

--- PhysioWat/designer/forms.py
+++ PhysioWat/designer/forms.py
@@ -1,7 +1,7 @@
 __author__ = 'stephen'
 from django import forms
 from django.forms import ModelForm
-from PhysioWat.models import Experiment
+from PhysioWat.models import PhysiowatExperiment
 
 # FileUpload form class.
 class SensorDesignerForm(forms.Form):
@@ -14,7 +14,8 @@ class experiments(ModelForm):
     name = forms.CharField(widget=forms.TextInput())
     description = forms.CharField(widget=forms.Textarea())
     token = forms.CharField(widget=forms.PasswordInput())
+    repeat_token = forms.CharField(widget=forms.PasswordInput())
     class Meta:
-        model = Experiment
+        model = PhysiowatExperiment
         fields = ['name', 'description', 'token']
 
