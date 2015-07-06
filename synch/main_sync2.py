767e5ab5191caac2b79978377b471421a9f37bbb
RobertaMontagno <epson4000@hotmail.it>
2015-07-06 09:40:41 +0200

    Update main_sync2.py

--- synch/main_sync2.py
+++ synch/main_sync2.py
@@ -1,5 +1,5 @@
 import numpy as np
 import sync2 as s2
 
-s2.correlate_features("GSR__F.txt","GSR__m.txt")     #print the value of linear correlation GSR
-s2.correlate_features("EKG_F.txt","EKG_M.txt")       #print the value of linear correlation EKG
+s2.correlate_features("feat_GSR_F01_F.txt","feat_GSR_F01_M.txt")     #print the value of linear correlation GSR
+s2.correlate_features("feat_EKG_F01_F.txt"," feat_EKG_F01_M.txt")       #print the value of linear correlation EKG
