40c42e880b08f31c549d996b39f8a50f76c0612d
Illedran <nrd.nardelli@gmail.com>
2015-07-07 22:51:23 +0200

    some more views

--- PhysioWat/uploader/views.py
+++ PhysioWat/uploader/views.py
@@ -10,19 +10,22 @@ from django.contrib import messages
 def upload(request):
 
     if request.method == "POST":
-
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            experiment_id = request.POST.get('experiment')
-            actualPasscode = Experiment.objects.get(id=experiment_id).token
+            exp_id = request.POST.get('experiment')
+            actualPasscode = Experiment.objects.get(id=exp_id).token
             enteredPasscode = form.cleaned_data
             enteredPasscode = enteredPasscode["password"]
-
             if enteredPasscode == actualPasscode:
-                csvtodb.putintodbflex(request.FILES.getlist('file'), request.POST.get('device'), request.POST.get('description'), experiment_id)
-                messages.success(request, 'Successfully Uploaded File')
+                try:
+                    csvtodb.putintodbflex(request.FILES.getlist('file'), request.POST.get('device'),
+                                          request.POST.get('description'), exp_id)
+                    messages.success(request, 'Successfully Uploaded File!')
+
+                except Exception:
+                    messages.error(request, 'Invalid request. Make sure that the file to import is in csv/txt format.')
             else:
-                messages.error(request, 'Invalid Password')
+                messages.error(request, 'Wrong password.')
 
         return HttpResponseRedirect(reverse('user_upload'))
     else:
