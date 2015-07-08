85b7419413d1088f77099ffa7ff42794e5b167a0
Riccardo Mancini <rickyman7@gmail.com>
2015-07-08 16:51:40 +0200

    almost finished

--- PhysioWat/extfeat/urls.py
+++ PhysioWat/extfeat/urls.py
@@ -5,6 +5,6 @@ from .views import getAlgorithm, ml_input, select_experiment, select_record
 urlpatterns = patterns('extfeat.views',
     url( regex='^alg_selection/(?P<id_record>\d+)$', view=getAlgorithm, name="alg_choose" ),
     url( regex='^machine_learning/$', view=ml_input, name="ml_param_setting" ),
-    url(regex='^experiments/$', view=select_experiment, name='experiment_selector'),
-    url(regex='^records/(?P<id_num>\d+)/$', view=select_record, name='record_selector'),
+    url(regex='^experiments/$', view=select_experiment, name='extf_experiment_selector'),
+    url(regex='^records/(?P<id_num>\d+)/$', view=select_record, name='extf_record_selector'),
 )
