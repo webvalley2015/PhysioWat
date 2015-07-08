e33dc3f69edb94950af8eb4064f4c6a555f1153b
Emanuele Pallaoro <ema.pallaoro@gmail.com>
2015-07-08 15:39:20 +0200

    addedd form for id choosing in extfeat

--- PhysioWat/extfeat/urls.py
+++ PhysioWat/extfeat/urls.py
@@ -1,11 +1,11 @@
 __author__ = 'federico'
 from django.conf.urls import url, patterns
-from .views import getAlgorithm, ml_input, select_experiment, select_record
+from .views import getAlgorithm, ml_input, select_experiment, select_record, select_signal
 
 urlpatterns = patterns('extfeat.views',
     url( regex='^alg_selection/(?P<id_num>\d+)$', view=getAlgorithm, name="alg_choose" ),
     url( regex='^machine_learning/$', view=ml_input, name="ml_param_setting" ),
     url(regex='^experiments/$', view=select_experiment, name='experiment_selector'),
     url(regex='^records/(?P<id_num>\d+)/$', view=select_record, name='record_selector'),
-
+    url(regex='^signals/(?P<id_record>\d+)/$', view=select_signal, name='signal_selector'),
                        )
