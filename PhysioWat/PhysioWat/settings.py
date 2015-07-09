d325e76a63f45734b9f00b78a94f640363373184
Illedran <nrd.nardelli@gmail.com>
2015-07-09 19:05:45 +0200

    extreme programming rulez

--- PhysioWat/PhysioWat/settings.py
+++ PhysioWat/PhysioWat/settings.py
@@ -84,17 +84,6 @@ WSGI_APPLICATION = 'PhysioWat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'physiowat_db',
-            'USER': 'developer',
-            'PASSWORD': 'webvalley',
-            'HOST': '192.168.210.175',
-            'PORT': '5432',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
