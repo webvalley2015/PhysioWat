44316de8aeffa6052b749df944486fa9171ccfaf
filosi <filosi@fbk.eu>
2015-07-09 15:40:41 +0200

    fixed preprocessing graph

--- PhysioWat/preproc/graphs.py
+++ PhysioWat/preproc/graphs.py
@@ -34,34 +34,45 @@ class linegraph2(HighChartsMultiAxesView):
         urlTmp = self.kwargs
         # get data type list
         # vals = getavaliabledatavals(urlTmp['id_num'])
+        data = []
         if urlTmp['elab'] == "raw":
-            data = SensorRawData.objects.filter(recording_id=urlTmp['id_num']).order_by('id')
+            idx = [(0, 'raw')]
+            data.append(SensorRawData.objects.filter(recording_id=urlTmp['id_num']).order_by('id'))
             self.title = 'Raw Data'
         elif urlTmp['elab'] == "proc":
-            print urlTmp['id_num']
-            data = Preprocessed_Data.objects.filter(pp_recording_id=urlTmp['id_num']).order_by('id')
+            idx = Preprocessed_Recording.objects.filter(batch_id=urlTmp['id_num']).values_list("pk", "signal_type_name")
+            for i in idx:
+                data.append(Preprocessed_Data.objects.filter(pp_recording_id=i[0]).order_by('id'))
             self.title = 'Preprocessed Data'
 
         print "DEBUG data", data, len(data), type(data)
 
         self.yaxis = {'title': {'text': ''}}
-        data_tmp = [i.store for i in data]
-        self.series = []
-        tmp = {k: (map(float, map(itemgetter(k), data_tmp))) for k in data_tmp[0]}
-        # CALCULATING SAMPLING FREQ.
-        samling_freq = 0.0
-        for i in range(0, 10):
-            samling_freq = samling_freq + (tmp['TIME'][i + 1] - tmp['TIME'][i])
-        samling_freq = samling_freq / 10
-        samling_freq = int(round(1 / samling_freq))
-        samling_freq = "%d" % samling_freq
-        samling_freq = str(' sampling frequency: ' + str(samling_freq) + 'Hz')
-        self.subtitle = samling_freq
 
-        # ADDING TIMESTAMP
-        for k in tmp.keys():
-            tmplist = [[tmp['TIME'][i] * 1000, tmp[k][i]] for i in xrange(len(tmp[k]))]
-            self.series.append({'data': tmplist, 'name': k})
+        data_tmp = []
+        for d in data:
+            data_tmp.append([i.store for i in d])
+
+        self.series = []
+
+        for j, d in enumerate(data_tmp):
+            tmp = {k: (map(float, map(itemgetter(k), d))) for k in d[0]}
+
+
+            # CALCULATING SAMPLING FREQ.
+            samling_freq = 0.0
+            for i in range(0, 10):
+                samling_freq = samling_freq + (tmp['TIME'][i + 1] - tmp['TIME'][i])
+            samling_freq = samling_freq / 10
+            samling_freq = int(round(1 / samling_freq))
+            samling_freq = "%d" % samling_freq
+            samling_freq = str(' sampling frequency: ' + str(samling_freq) + 'Hz')
+            self.subtitle = samling_freq
+
+            # ADDING TIMESTAMP
+            for k in tmp.keys():
+                tmplist = [[tmp['TIME'][i] * 1000, tmp[k][i]] for i in xrange(len(tmp[k]))]
+                self.series.append({'data': tmplist, 'name': '{}_{}'.format(k, str(idx[j][1]))})
         # print times
 
         # print "fsahdiasdjasdioadsjdoisj"
