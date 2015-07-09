d71487d7acec83bca9990a39e2e86ebba56b9551
Stephen Mylabathula <mylo777@icloud.com>
2015-07-09 11:48:07 +0200

    fixed minor bug in ML, but still getting file error and shape error. We somehow need to be able to select the exact id of each pp_recording. Not just the general, because there are multiple pp_recordings.

--- PhysioWat/preproc/scripts/processing_scripts/pddbload.py
+++ PhysioWat/preproc/scripts/processing_scripts/pddbload.py
@@ -6,8 +6,8 @@ from PhysioWat.models import FeatExtractedData
 
 
 def load_file_pd_db(recordingID):
-    #fpath = FeatExtractedData.objects.filter(pp_recording=recordingID).values_list('path_to_file') #[0][0]
-    fpath = FeatExtractedData.objects.get(pp_recording=recordingID).path_to_file.distinct()
+    fpath = FeatExtractedData.objects.filter(pp_recording=recordingID).values_list('path_to_file') #[0][0]
+    #fpath = FeatExtractedData.objects.get(pp_recording=recordingID).path_to_file.distinct()
     uncleanedfile = open(fpath, 'r')
     if os.path.isfile(fpath + 'p'):
         uncleanedfile.close()
