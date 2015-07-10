54fe416bcb7b2c9732c9cb343a59467ccc506b07
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2015-07-10 13:31:09 +0200

    media root settings fix

--- PhysioWat/preproc/scripts/processing_scripts/pddbload.py
+++ PhysioWat/preproc/scripts/processing_scripts/pddbload.py
@@ -8,7 +8,7 @@ from django.conf import  settings
 def load_file_pd_db(recordingID):
     fpath = FeatExtractedData.objects.filter(id=recordingID).values_list('path_to_file') #[0][0]
     #fpath = FeatExtractedData.objects.get(pp_recording=recordingID).path_to_file.distinct()
-    file_name = '{0}/{1}'.format(settings.BASE_DIR, fpath[0][0])
+    file_name = '{0}/{1}'.format(settings.MEDIA_ROOT, fpath[0][0])
     uncleanedfile = open(file_name, 'r')
 
     file = open(file_name + 'proc_ml', 'w')
